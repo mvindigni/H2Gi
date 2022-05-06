11b83dca800b82fafe8bf39fee560a2e90ad827d
Matt <mrvindigni@wpi.edu>
2022-05-05 21:49:47 -0400

    youtube link support

--- disabled-commands/kepler.js
+++ disabled-commands/kepler.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('kepler')
+		.setDescription('...'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply('PettingZooWPI.aternos.me');
 	},
 };
